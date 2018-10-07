b4baaa7c37ea931947d6ae7b60791cfece77f09c
popunbom <fantom0779@gmail.com>
2018-10-07 14:10:41 +0900

    Change methods to install zsh plugins to 'zplug'

--- setup.sh
+++ setup.sh
@@ -4,18 +4,11 @@ OLD_IFS=$IFS
 IFS=$'\n'
 
 SCRIPT_DIR="$(cd "$(dirname $0)"; pwd)"
-PATH_DOTFILES="${SCRIPT_DIR}"
-PATH_FILE_LISTS="${SCRIPT_DIR}/symlinks.list"
-PATH_SYMLINK_FROM="${SCRIPT_DIR}"
+PATH_DOTFILES="${SCRIPT_DIR}/shims"
+PATH_SYMLINK_FROM="${PATH_DOTFILES}"
 PATH_SYMLINK_TO="${HOME}"
 
 
-
-if [[ ! -e $PATH_FILE_LISTS ]]; then
-	echo "File not found! -- ${PATH_FILE_LISTS}"
-	touch ${PATH_FILE_LISTS}
-	echo "Please add filename to \"${PATH_FILE_LISTS}\""
-else
 
 cat << EOF
 =============================================================
@@ -28,7 +21,7 @@ cat << EOF
 ╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
 
 =============================================================
-============= DOTFILES ENVIRONMENT INSTALLER ================
+=================== DOTFILES  INSTALLER =====================
 =============================================================
 
 PLEASE CHECK DIRECTORY PATH
@@ -37,63 +30,62 @@ PLEASE CHECK DIRECTORY PATH
 
 EOF
 
-	echo -n "IS IT CORRECT [y/n] ?  "
-	read answer
-	while [[ "$answer" != "y" && "$answer" != "n" ]]; do
-		echo -n "Please input 'y' or 'n': "
-		read answer
-	done
+echo -n "IS IT CORRECT [y/n] ?  "
+read answer < /dev/tty
+while [[ "$answer" != "y" && "$answer" != "n" ]]; do
+	echo -n "Please input 'y' or 'n': "
+	read answer < /dev/tty
+done
+
+if [[ "$answer" == "y" ]]; then
 
 	# <-- Main Routine
-	if [[ "$answer" == "y" ]]; then
-		while read BASENAME_DOTFILE; do
-			echo $BASENAME_DOTFILE
+	for BASENAME_DOTFILE in $(ls -a1 ${PATH_DOTFILES} | grep -Ev "^\.+$"); do
+		echo $BASENAME_DOTFILE
 
-			# Set symlink
-			SYMLINK_FROM="${PATH_SYMLINK_FROM}/${BASENAME_DOTFILE}"
-			echo "SYMLINK_FROM: ${SYMLINK_FROM}"
-			SYMLINK_TO="${PATH_SYMLINK_TO}/.${BASENAME_DOTFILE}"
-			echo "SYMLINK_TO  : ${SYMLINK_TO}"
+		# Set symlink
+		SYMLINK_FROM="${PATH_SYMLINK_FROM}/${BASENAME_DOTFILE}"
+		echo "SYMLINK_FROM: ${SYMLINK_FROM}"
+		SYMLINK_TO="${PATH_SYMLINK_TO}/.${BASENAME_DOTFILE}"
+		echo "SYMLINK_TO  : ${SYMLINK_TO}"
 
-			# Check "SYMLINK_FROM" existance
-			if [[ ! -e $SYMLINK_FROM ]]; then
-				echo "${SYMLINK_FROM}: file not found! -- skipped"
+		# Check "SYMLINK_FROM" existance
+		if [[ ! -e $SYMLINK_FROM ]]; then
+			echo "${SYMLINK_FROM}: file not found! -- skipped"
 
-			else
-				# Check "SYMLINK_TO" existance
-				if [[ -e $SYMLINK_TO ]]; then
-					echo -ne "${SYMLINK_TO}: already exists! \n  Do you want to overwrite [y/n] ? "
+		else
+			# Check "SYMLINK_TO" existance
+			if [[ -e $SYMLINK_TO || -h $SYMLINK_TO ]]; then
+				echo -ne "${SYMLINK_TO}: already exists! \n  Do you want to overwrite [y/n] ? "
 
-					read answer
-					while [[ "$answer" != "y" && "$answer" != "n" ]]; do
-						echo -n "Please input 'y' or 'n': "
-						read answer
-					done
+				read answer < /dev/tty
+				while [[ "$answer" != "y" && "$answer" != "n" ]]; do
+					echo -n "Please input 'y' or 'n': "
+					read answer < /dev/tty
+				done
 
-					if [[ "$answer" == "y" ]]; then
-						echo "${SYMLINK_TO}: delete and create symlink"
-						# Delete & Create symlink
-						rm -rf $SYMLINK_TO
-						ln -s ${SYMLINK_FROM} ${SYMLINK_TO}
-					elif [[ "$answer" == "n" ]]; then
-						echo "${SYMLINK_TO}: not changed."
-					fi
-
-				else
-					echo "${SYMLINK_TO}: create symlink"	
-					# Create symlink
+				if [[ "$answer" == "y" ]]; then
+					echo "${SYMLINK_TO}: delete and create symlink"
+					# Delete & Create symlink
+					rm -rf $SYMLINK_TO
 					ln -s ${SYMLINK_FROM} ${SYMLINK_TO}
+				elif [[ "$answer" == "n" ]]; then
+					echo "${SYMLINK_TO}: not changed."
 				fi
 
+			else
+				echo "${SYMLINK_TO}: create symlink"	
+				# Create symlink
+				ln -s ${SYMLINK_FROM} ${SYMLINK_TO}
 			fi
-
-		done < $PATH_FILE_LISTS
-
-	elif [[ "$answer" == "n" ]]; then
-		echo "SCRIPT FINISHED WITHOUT ANY FILE CHANGE"
-	fi
+		fi
+	done
 	# --> Main Routine
+
+elif [[ "$answer" == "n" ]]; then
+	echo "SCRIPT FINISHED WITHOUT ANY FILE CHANGE"
 fi
 
 
 IFS=$OLD_IFS
+# TEST STRING
