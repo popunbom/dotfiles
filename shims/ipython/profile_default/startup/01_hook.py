484a741010093ae186fadd801c0a4f3eca17c0d2
popunbom <fantom0779@gmail.com>
2019-06-11 11:32:01 +0900

    Edit: 01_hook.py - 画像表示用のマジックコマンドを追加

--- shims/ipython/profile_default/startup/01_hook.py
+++ shims/ipython/profile_default/startup/01_hook.py
@@ -6,13 +6,15 @@
 import os
 import sys
 from common_proc import colors
-from IPython.core.magic import register_line_cell_magic
+from IPython.core.magic import register_line_magic, register_line_cell_magic
 if sys.platform == 'darwin':
     import subprocess
 elif sys.platform.startswith('linux'):
     from subprocess import Popen, PIPE
 else:
     raise ImportError("Clip magic only works on osx or linux!")
+
+import matplotlib.pyplot as plt
 
 
 def _copy_to_clipboard(arg):
@@ -30,6 +32,23 @@ def _copy_to_clipboard(arg):
     print(colors(' -- Copied to clipboard!', attr='bold', fg='green'))
 
 
+def _pyplot_imshow(img):
+    if type(img) != np.ndarray:
+        print(f"argument type must be numpy.ndarray, not {type(img)} !")
+        return
+
+    plt.tick_params(
+        labelbottom=False, labelleft=False, labelright=False, labeltop=False,
+        bottom=False, left=False, right=False, top=False
+    )
+    if img.ndim == 2:
+        plt.imshow(img, cmap="gray")
+    else:
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    plt.show()
+
+
 @register_line_cell_magic
 def copy(line, cell=None):
     if line and cell:
@@ -38,8 +57,28 @@ def copy(line, cell=None):
     _copy_to_clipboard(cell or line)
 
 
-print(colors("ADD", attr='bold', fg='white'), colors("%copy", attr='bold',
-                                                     fg='green'), colors("BUILT-IN MAGGIC COMMAND !", attr='bold', fg='white'))
+@register_line_magic
+def imshow(line):
+    img = eval(line)
+
+    if img is None:
+        print(f"UndefinedVariable: '{line}'")
+    else:
+        _pyplot_imshow(img)
+
+
+print(
+    colors("ADD", attr='bold', fg='white'),
+    colors("%copy", attr='bold', fg='green'),
+    colors("BUILT-IN MAGGIC COMMAND !", attr='bold', fg='white')
+)
+
+print(
+    colors("ADD", attr='bold', fg='white'),
+    colors("%imshow", attr='bold', fg='green'),
+    colors("BUILT-IN MAGGIC COMMAND !", attr='bold', fg='white')
+)
 
 # We delete it to avoid name conflicts for automagic to work
 del copy
+del imshow
