f39d147c221eea88708627bfdb2f9372c2b5f135
popunbom <28749104+popunbom@users.noreply.github.com>
2024-10-16 22:19:43 +0900

    Migrate to mise

--- zsh/scripts/paths.zsh
+++ zsh/scripts/paths.zsh
@@ -1,8 +1,6 @@
 #!/bin/zsh
 
-export PATH="/usr/local/bin${PATH+:${PATH}}"
-export PATH="${HOME}/.rd/bin${PATH+:${PATH}}"
-export PATH="${HOME}/.golang/bin${PATH+:${PATH}}"
-export PATH="${HOME}/.nodenv/shims${PATH+:${PATH}}"
-export PATH="${HOME}/.pyenv/shims${PATH+:${PATH}}"
-export PATH="${HOME}/.jenv/shims${PATH+:${PATH}}"
+export PATH="/usr/local/bin:/usr/local/sbin:${PATH+:${PATH}}"
+
+[[ -d ${HOME}/.rd ]] && export PATH="${HOME}/.rd/bin${PATH+:${PATH}}"
+[[ -d ${HOME}/.golang ]] && export PATH="${HOME}/.golang/bin${PATH+:${PATH}}"
